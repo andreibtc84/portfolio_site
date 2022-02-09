97448f776ff877c649242b006e1558597c41f7ef
Andrei Btc <andreibtc@gmail.com>
2022-02-09 19:08:50 +0000

    Adds transparency on scroll to navbar /portfolio section / styling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 console.log("Connected!");
+
+const navbar = document.querySelector(".navbar");
+const navTransparency = () => {
+  scrollY === 0
+    ? (navbar.style.backgroundColor = "#424b5401")
+    : (navbar.style.backgroundColor = "#424b54ff");
+};
+
+document.addEventListener("scroll", navTransparency);

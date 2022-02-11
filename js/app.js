edd2c683b609877f7b80ccca715bcc7ee2f87617
Andrei Btc <andreibtc@gmail.com>
2022-02-11 19:55:55 +0000

    Adds responsive navbar with burger menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 console.log("Connected!");
 
-// Navigation
+// Navigation start
+// Transparency on scroll
 
 const navbar = document.querySelector(".navbar");
 const navTransparency = () => {
@@ -10,3 +11,33 @@ const navTransparency = () => {
 };
 
 document.addEventListener("scroll", navTransparency);
+
+// Responsive burger menu
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navList = document.querySelector(".nav-list");
+  const navItems = document.querySelectorAll(".nav-item");
+
+  burger.addEventListener("click", () => {
+    //Toggles Navigation
+    navList.classList.toggle("nav-active");
+
+    //Animates Links
+    navItems.forEach((item, index) => {
+      if (item.style.animation) {
+        item.style.animation = "";
+      } else {
+        item.style.animation = `navItemFade 0.5s ease forwards ${
+          index / 7 + 0
+        }s`;
+      }
+    });
+
+    //Burger animation
+    burger.classList.toggle("burgerToX");
+  });
+};
+navSlide();
+
+// Navigation end
